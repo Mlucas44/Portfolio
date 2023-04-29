70ade290860a505bfd61dcfe761958dfa61269d6
Lucas <Mlucas44@outlook.fr>
2023-04-29 13:27:27 +0200

    upgrade

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,10 @@
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+		bio: '	Je suis Lucas Metayer, un développeur web passionné et ambitieux. Actuellement en alternance chez SDVI, je travaille en tant que développeur web full stack, utilisant des technologies telles que PHP 8, Symfony 6, Bootstrap et Stimulus pour réaliser des missions données par la chef de projet.',
 	},
 	{
 		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+		bio: 'Je suis convaincu que le développement web est un domaine en constante évolution et je suis toujours à la recherche de nouvelles connaissances et compétences pour rester à la pointe des dernières technologies. Mon objectif est de devenir un développeur web expérimenté et innovant, capable de résoudre des problèmes complexes et de créer des solutions originales.'
 	},
 ];
