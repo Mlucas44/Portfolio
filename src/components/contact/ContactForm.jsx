767f9a699beaa3668f05bc3b9fe76eeb2721ce7c
Lucas Metayer <Mlucas44@outlook.fr>
2024-02-29 08:49:35 +0100

    update

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,33 +1,10 @@
 const ContactForm = () => {
   return (
     <form name="contact" method="POST" data-netlify="true">
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Role:{" "}
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+      <input type="text" name="name" placeholder="Votre nom" />
+      <input type="email" name="email" placeholder="Votre email" />
+      <textarea name="message" placeholder="Votre message"></textarea>
+      <button type="submit">Envoyer</button>
     </form>
   );
 };
