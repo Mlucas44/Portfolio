de5bcb9c5559d7516449e9ae621ae48dfa0bba5b
Lucas Metayer <Mlucas44@outlook.fr>
2024-02-29 08:55:36 +0100

    update

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,6 +1,6 @@
 const ContactForm = () => {
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true" action="/">
       <input type="text" name="name" placeholder="Votre nom" />
       <input type="email" name="email" placeholder="Votre email" />
       <textarea name="message" placeholder="Votre message"></textarea>
