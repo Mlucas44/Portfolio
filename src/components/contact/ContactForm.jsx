2950381af49e759d95c2e2f49be66dfebaba0e17
Lucas Metayer <Mlucas44@outlook.fr>
2024-02-29 08:47:24 +0100

    update

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,35 +1,33 @@
 const ContactForm = () => {
   return (
-    <form
-      name="contact"
-      method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="form-name" value="contact" />
-      <p>Formulaire de contact</p>
-
-      <label htmlFor="name">Nom complet</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        placeholder="Votre nom, prénom"
-      />
-
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" placeholder="Votre email" />
-
-      <label htmlFor="subject">Sujet</label>
-      <input type="text" id="subject" name="subject" placeholder="Sujet" />
-
-      <label htmlFor="message">Message</label>
-      <textarea id="message" name="message"></textarea>
-
-      {/* Vous ne verrez pas ce champ dans le formulaire, il est utilisé pour éviter le spam */}
-      <input type="hidden" name="bot-field" />
-
-      <button type="submit">Envoyer un message</button>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:{" "}
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   );
 };
